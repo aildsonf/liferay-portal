204bf0a29d3f462f136844efe41fd782dd8540b9
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-02 18:04:40 -0300

    LPS-140164 updated modal props and rendering

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ModalCardSubscription/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ModalCardSubscription/index.js
@@ -1,88 +1,194 @@
+
+import { useQuery } from '@apollo/client';
 import ClayButton from '@clayui/button';
 import ClayIcon from '@clayui/icon';
 import ClayModal from '@clayui/modal';
 import ClayTable from '@clayui/table';
-import { status } from '../../utils/constants';
+import React, {useState} from 'react';
+import {getAccountSubscriptionsTerms} from '../../../../common/services/liferay/graphql/queries';
+import {status} from '../../utils/constants';
 import StatusTag from '../StatusTag';
+import SubscriptionTermsModalPagination from '../SubscriptionTermsModalPagination';
 
+const ModalCardSubscription = ({accountSubscriptionERC, observer, onClose}) => {
+	const [activePage, setActivePage] = useState(1);
+	const MAX_ITEMS_PER_PAGE = 5;
 
-const ModalCardSubscription = ({ observer, onClose }) => {
+	const parseDate = (rawDate) => {
+		const date = new Date(rawDate);
+		const month = date.getMonth() + 1;
+		const day = date.getDate();
+		const year = date.getFullYear();
 
-  return (
-    <>
-      <ClayModal
-        observer={observer}
-        size="lg"
-      >
+		return `${day}/${month < 10 ? `0${month}` : month}/${year}`;
+	};
 
-        <div className="p-4">
-          <div className="d-flex justify-content-between mb-4 teste-cursor">
-            <div className="flex-row mb-1">
-              <h6 className="text-brand-primary">Subscription Terms</h6>
+	const {
+		data: subscriptionsTerms,
+		loading: isSubscriptionsTermsLoading,
+	} = useQuery(getAccountSubscriptionsTerms, {
+		variables: {
+			filter: `accountSubscriptionERC eq '${accountSubscriptionERC}'`,
+			page: activePage,
+			pageSize: MAX_ITEMS_PER_PAGE,
+		},
+	});
 
-              <h2 className="text-neutral-10">DXP Production</h2>
-            </div>
+	const accountSubscriptionTermsItems =
+		subscriptionsTerms?.c?.accountSubscriptionTerms?.items || [];
 
-            <ClayButton
-              aria-label="close"
-              className="close"
-              displayType="unstyled"
-              onClick={onClose}
-            >
-              <ClayIcon
-                symbol="times"
-              />
-            </ClayButton>
-          </div>
+	return (
+		<>
+			{!isSubscriptionsTermsLoading && (
+					<ClayModal center={true} observer={observer} size="lg">
+						<div className="pt-4 px-4">
+							<div className="d-flex justify-content-between mb-4 teste-cursor">
+								<div className="flex-row mb-1">
+									<h6 className="text-brand-primary">
+										SUBSCRIPTION TERMS
+									</h6>
 
-          <div>
-            <ClayTable tableVerticalAlignment="middle">
-              <ClayTable.Head>
-                <ClayTable.Row>
+									<h2 className="text-neutral-10">
+										DXP Production
+									</h2>
+								</div>
 
-                  <ClayTable.Cell align="center" className="bg-neutral-1 font-weight-bold text-neutral-8" expanded headingCell>Start-End Date</ClayTable.Cell>
+								<ClayButton
+									aria-label="close"
+									className="close"
+									displayType="unstyled"
+									onClick={onClose}
+								>
+									<ClayIcon symbol="times" />
+								</ClayButton>
+							</div>
 
-                  <ClayTable.Cell align="center" className="bg-neutral-1 font-weight-bold text-neutral-8" headingCell>Provisioned</ClayTable.Cell>
+							<div>
+								<ClayTable tableVerticalAlignment="middle">
+									<ClayTable.Head>
+										<ClayTable.Row>
+											<ClayTable.Cell
+												align="center"
+												className="bg-neutral-1 font-weight-bold text-neutral-8"
+												expanded
+												headingCell
+											>
+												Start-End Date
+											</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" className="bg-neutral-1 font-weight-bold text-neutral-8" headingCell>Purchased</ClayTable.Cell>
+											<ClayTable.Cell
+												align="center"
+												className="bg-neutral-1 font-weight-bold text-neutral-8"
+												headingCell
+											>
+												Provisioned
+											</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" className="bg-neutral-1 font-weight-bold text-neutral-8" headingCell>Instance Size</ClayTable.Cell>
+											<ClayTable.Cell
+												align="center"
+												className="bg-neutral-1 font-weight-bold text-neutral-8"
+												headingCell
+											>
+												Purchased
+											</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" className="bg-neutral-1 font-weight-bold text-neutral-8" headingCell>Status</ClayTable.Cell>
+											<ClayTable.Cell
+												align="center"
+												className="bg-neutral-1 font-weight-bold text-neutral-8"
+												headingCell
+											>
+												Instance Size
+											</ClayTable.Cell>
 
-                </ClayTable.Row>
-              </ClayTable.Head>
+											<ClayTable.Cell
+												align="center"
+												className="bg-neutral-1 font-weight-bold text-neutral-8"
+												headingCell
+											>
+												Status
+											</ClayTable.Cell>
+										</ClayTable.Row>
+									</ClayTable.Head>
 
-              <ClayTable.Body>
+									<ClayTable.Body>
+										{accountSubscriptionTermsItems.map(
+											(item) => (
+												<>
+													<ClayTable.Row>
+														<ClayTable.Cell align="center">
+															{`${parseDate(
+																item.startDate
+															)} - ${parseDate(
+																item.endDate
+															)}`}
+														</ClayTable.Cell>
 
-                <ClayTable.Row>
-                  <ClayTable.Cell align="center" >6/10/2018 - 6/10/2021</ClayTable.Cell>
+														<ClayTable.Cell align="center">
+															{item?.provisioned ||
+																'-'}
+														</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" >3</ClayTable.Cell>
+														<ClayTable.Cell align="center">
+															{item.quantity}
+														</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" >5</ClayTable.Cell>
+														<ClayTable.Cell align="center">
+															{item.instanceSize}
+														</ClayTable.Cell>
 
-                  <ClayTable.Cell align="center" >1</ClayTable.Cell>
+														<ClayTable.Cell align="center">
+															{item.subscriptionTermStatus ===
+																'Active' && (
+																<StatusTag
+																	currentStatus={
+																		status.active
+																	}
+																/>
+															)}
 
-                  <ClayTable.Cell align="center" >
+															{item.subscriptionTermStatus ===
+																'Expired' && (
+																<StatusTag
+																	currentStatus={
+																		status.expired
+																	}
+																/>
+															)}
 
-                    <StatusTag currentStatus={status.active} />
+															{item.subscriptionTermStatus ===
+																'Future' && (
+																<StatusTag
+																	currentStatus={
+																		status.future
+																	}
+																/>
+															)}
+														</ClayTable.Cell>
+													</ClayTable.Row>
+												</>
+											)
+										)}
+									</ClayTable.Body>
+								</ClayTable>
+							</div>
+						</div>
 
-                  </ClayTable.Cell>
-                </ClayTable.Row>
-
-              </ClayTable.Body>
-            </ClayTable>
-
-          </div>
-        </div>
-
-
-      </ClayModal>
-    </>
-  )
-}
+						{accountSubscriptionTermsItems.length >
+						MAX_ITEMS_PER_PAGE ? (
+							<SubscriptionTermsModalPagination
+								activePage={activePage}
+								setActivePage={setActivePage}
+								totalItems={
+									accountSubscriptionTermsItems.length
+								}
+							/>
+						) : (
+							<p className="mb-4 mx-4 text-paragraph">{`Showing 1 to ${accountSubscriptionTermsItems.length} of ${accountSubscriptionTermsItems.length} entries.`}</p>
+						)}
+					</ClayModal>
+			)}
+		</>
+	);
+};
 
 export default ModalCardSubscription;
-
-
