164f4f4ba810b532f5bc1c1ec4c00c2ec59545a4
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-02 17:55:40 -0300

    LPS-140164 refactored page's child components

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Overview/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Overview/index.js
@@ -1,24 +1,18 @@
 import {useQuery} from '@apollo/client';
-import {useContext, useEffect, useState} from 'react';
+import {useContext, useEffect} from 'react';
 import {
 	onboardingPageGuard,
 	overviewPageGuard,
 	usePageGuard,
 } from '../../../../common/hooks/usePageGuard';
-import {
-	getAccountSubscriptionGroupsByFilter,
-	getKoroneikiAccounts,
-} from '../../../../common/services/liferay/graphql/queries';
-import SubscriptionsBar from '../../components/SubscriptionsBar';
-import SubscriptionsCards from '../../components/SubscriptionsCards';
+import {getKoroneikiAccounts} from '../../../../common/services/liferay/graphql/queries';
+import Subscriptions from '../../components/Subscriptions';
 import {AppContext} from '../../context';
 import {actionTypes} from '../../context/reducer';
 import {CUSTOM_EVENTS} from '../../utils/constants';
 
 const Overview = ({userAccount}) => {
 	const [{project}, dispatch] = useContext(AppContext);
-	const [subscriptionsTags, setSubscriptionsTags] = useState(() => []);
-	const [selectedTag, setSelectedTag] = useState(() => '');
 
 	const {isLoading} = usePageGuard(
 		userAccount,
@@ -35,15 +29,6 @@ const Overview = ({userAccount}) => {
 			},
 		}
 	);
-
-	const {
-		data: accountSubscriptionsData,
-		loading: isAccountSubscriptionsLoading,
-	} = useQuery(getAccountSubscriptionGroupsByFilter, {
-		variables: {
-			filter: `accountKey eq '${project.accountKey}'`,
-		},
-	});
 
 	useEffect(() => {
 		if (!isLoading && data) {
@@ -64,35 +49,14 @@ const Overview = ({userAccount}) => {
 		}
 	}, [data, dispatch, isLoading]);
 
-	useEffect(() => {
-		if (!isAccountSubscriptionsLoading && accountSubscriptionsData) {
-			setSubscriptionsTags(
-				accountSubscriptionsData?.c?.accountSubscriptionGroups?.items ||
-					[]
-			);
-		}
-	}, [accountSubscriptionsData, isAccountSubscriptionsLoading]);
-
 	if (isLoading || isLoadingKoroneiki) {
 		return <div>Overview Skeleton</div>;
 	}
 
 	return (
-		<div className="d-flex flex-column mx-4">
-			Overview Page
-			{!isAccountSubscriptionsLoading && (
-				<SubscriptionsBar
-					selectedTag={selectedTag}
-					setSelectedTag={setSelectedTag}
-					subscriptionsTags={subscriptionsTags}
-				/>
-			)}
-
-			<SubscriptionsCards
-				accountKey={project.accountKey}
-				selectedTag={selectedTag}
-			/>
-		</div>
+		<>
+			<Subscriptions accountKey={project.accountKey} />
+		</>
 	);
 };
 
