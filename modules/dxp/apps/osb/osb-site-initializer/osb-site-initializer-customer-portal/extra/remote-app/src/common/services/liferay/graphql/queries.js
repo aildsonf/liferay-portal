1d4925e0a428123eebc496c7cb24b14390e5ba2e
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-09 10:55:37 -0300

    LPS-140164 Added new queries for Subscriptions

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/queries.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/queries.js
@@ -1,8 +1,71 @@
 import {gql} from '@apollo/client';
 
-export const getUserAccount = gql`
-	query getUserAccount($id: Long!) {
-		userAccount(userAccountId: $id) {
+export const getAccountSubscriptionsTerms = gql`
+	query accountSubscriptionsTerms(
+		$filter: String
+		$page: Int
+		$pageSize: Int
+	) {
+		c {
+			accountSubscriptionTerms(
+				filter: $filter
+				page: $page
+				pageSize: $pageSize
+			) {
+				totalCount,
+				items {
+					accountKey
+					accountSubscriptionERC
+					accountSubscriptionGroupERC
+					accountSubscriptionTermId
+					c_accountSubscriptionTermId
+					endDate
+					instanceSize
+					quantity
+					startDate
+					subscriptionTermStatus
+				}
+			}
+		}
+	}
+`;
+
+export const getAccountSubscriptionGroupsByFilter = gql`
+	query accountSubscriptionGroups($filter: String) {
+		c {
+			accountSubscriptionGroups(filter: $filter) {
+				items {
+					name
+				}
+			}
+		}
+	}
+`;
+
+export const getAccountSubscriptions = gql`
+	query accountSubscriptions($filter: String) {
+		c {
+			accountSubscriptions(filter: $filter) {
+				items {
+					accountKey
+					accountSubscriptionId
+					c_accountSubscriptionId
+					endDate
+					instanceSize
+					quantity
+					startDate
+					name
+					accountSubscriptionGroupERC
+					subscriptionStatus
+				}
+			}
+		}
+	}
+`;
+
+export const getUserAccountById = gql`
+	query userAccount($userAccountId: Long) {
+		userAccount(userAccountId: $userAccountId) {
 			id
 			name
 			image
@@ -17,7 +80,7 @@ export const getUserAccount = gql`
 `;
 
 export const getDXPCDataCenterRegions = gql`
-	query getDXPCDataCenterRegions {
+	query dXPCDataCenterRegions {
 		c {
 			dXPCDataCenterRegions {
 				items {
@@ -30,9 +93,23 @@ export const getDXPCDataCenterRegions = gql`
 `;
 
 export const getKoroneikiAccounts = gql`
-	query getKoroneikiAccounts($filter: String) {
+	query koroneikiAccounts(
+		$aggregation: [String]
+		$filter: String
+		$page: Int = 1
+		$pageSize: Int = 20
+		$search: String
+		$sort: String
+	) {
 		c {
-			koroneikiAccounts(filter: $filter) {
+			koroneikiAccounts(
+				aggregation: $aggregation
+				filter: $filter
+				page: $page
+				pageSize: $pageSize
+				search: $search
+				sort: $sort
+			) {
 				items {
 					accountKey
 					code
@@ -50,9 +127,9 @@ export const getKoroneikiAccounts = gql`
 	}
 `;
 
-export const getAccountRolesAndAccountFlags = gql`
-	query getAccountRolesAndAccountFlags(
-		$accountId: Long!
+export const pageGuard = gql`
+	query accountRolesAndAccountFlags(
+		$accountId: Long
 		$accountFlagsFilter: String
 	) {
 		accountAccountRoles(accountId: $accountId) {
